9e84baa251eed840005c08fa42f0e3e371f56aaf
FarzamTP <farzam.taghipour@gmail.com>
2021-03-11 17:17:08 +0330

    routes and swahher.json added

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, make_response, jsonify
 from settings import *
+from routes import request_api
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 APP = Flask(__name__)
@@ -21,6 +23,21 @@ def handle_error_404(error):
 @APP.errorhandler(500)
 def handle_error_500(error):
     return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
+### swagger specific ###
+SWAGGER_URL = '/ui'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+APP.register_blueprint(request_api.get_blueprint())
 
 if __name__ == "__main__":
     APP.run(host=HOST, port=PORT)
--- routes/request_api.py
+++ routes/request_api.py
@@ -1,6 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, abort, request, jsonify
+import requests, os
 
 
 REQUEST_API = Blueprint('request_api', __name__)
 
-REQUEST_API.route()
+
+def get_blueprint():
+    """Return the blueprint for the main app module"""
+    return REQUEST_API
+
+
+@REQUEST_API.route('/classify', methods=['POST'])
+def get_target_image_url():
+    """
+    @param url: post : The target image url.
+    @returns: 201 : a label (cat / dog) as a flask/response object.
+    @raise 400: if unable to download the image.
+    """
+    if not request.get_json():
+        abort(400)
+    else:
+        data = request.get_json(force=True)
+        if not data.get('image_url'):
+            abort(400)
+        else:
+            url = data.get('image_url')
+            r = requests.get(url)
+            image_dir = f"media/{str(url).split('/')[-1]}"
+            if not os.path.exists(image_dir):
+                image = open(image_dir, "wb")
+                image.write(r.content)
+                image.close()
+                return jsonify({"image_dir": image_dir,
+                                "status": 200})
+            else:
+                return jsonify({"image_dir": image_dir,
+                                "status": 201})
